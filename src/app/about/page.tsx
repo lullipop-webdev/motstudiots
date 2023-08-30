07a79f5f57d05a18e202aadc3e4180302caf3883
Jonathan <jjames422@me.com>
2023-08-30 13:08:16 -0400

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,232 @@
+import { type Metadata } from 'next'
+import Image from 'next/image'
+
+import { Border } from '@/components/Border'
+import { ContactSection } from '@/components/ContactSection'
+import { Container } from '@/components/Container'
+import { FadeIn, FadeInStagger } from '@/components/FadeIn'
+import { GridList, GridListItem } from '@/components/GridList'
+import { PageIntro } from '@/components/PageIntro'
+import { PageLinks } from '@/components/PageLinks'
+import { SectionIntro } from '@/components/SectionIntro'
+import { StatList, StatListItem } from '@/components/StatList'
+import imageAngelaFisher from '@/images/team/angela-fisher.jpg'
+import imageBenjaminRussel from '@/images/team/benjamin-russel.jpg'
+import imageBlakeReid from '@/images/team/blake-reid.jpg'
+import imageChelseaHagon from '@/images/team/chelsea-hagon.jpg'
+import imageDriesVincent from '@/images/team/dries-vincent.jpg'
+import imageEmmaDorsey from '@/images/team/emma-dorsey.jpg'
+import imageJeffreyWebb from '@/images/team/jeffrey-webb.jpg'
+import imageKathrynMurphy from '@/images/team/kathryn-murphy.jpg'
+import imageLeonardKrasner from '@/images/team/leonard-krasner.jpg'
+import imageLeslieAlexander from '@/images/team/leslie-alexander.jpg'
+import imageMichaelFoster from '@/images/team/michael-foster.jpg'
+import imageWhitneyFrancis from '@/images/team/whitney-francis.jpg'
+import { loadArticles } from '@/lib/mdx'
+
+function Culture() {
+  return (
+    <div className="mt-24 rounded-4xl bg-neutral-950 py-24 sm:mt-32 lg:mt-40 lg:py-32">
+      <SectionIntro
+        eyebrow="Our culture"
+        title="Balance your passion with your passion for life."
+        invert
+      >
+        <p>
+          We are a group of like-minded people who share the same core values.
+        </p>
+      </SectionIntro>
+      <Container className="mt-16">
+        <GridList>
+          <GridListItem title="Loyalty" invert>
+          Our team has been with us since the beginning because we believe build better technology
+            starts with investing in our employees first.
+          </GridListItem>
+          <GridListItem title="Trust" invert>
+          Listening to our employees allowed us to craft amazing site for some top tier company's by
+           embracing remote opprotunities for our staff.
+          </GridListItem>
+          <GridListItem title="Compassion" invert>
+          Listen. That is the first step in showing your team how you care about their daily lives 
+            inside, and outside of the work place.
+          </GridListItem>
+        </GridList>
+      </Container>
+    </div>
+  )
+}
+
+const team = [
+  {
+    title: 'Leadership',
+    people: [
+      {
+        name: 'Leslie Alexander',
+        role: 'COO',
+        image: { src: imageLeslieAlexander },
+      },
+      {
+        name: 'Michael Foster',
+        role: 'CTO',
+        image: { src: imageMichaelFoster },
+      },
+      {
+        name: 'Dries Vincent',
+        role: 'CMO',
+        image: { src: imageDriesVincent },
+      },
+    ],
+  },
+  {
+    title: 'Team',
+    people: [
+      {
+        name: 'Chelsea Hagon',
+        role: 'Senior Developer',
+        image: { src: imageChelseaHagon },
+      },
+      {
+        name: 'Emma Dorsey',
+        role: 'Senior Designer',
+        image: { src: imageEmmaDorsey },
+      },
+      {
+        name: 'Leonard Krasner',
+        role: 'VP, User Experience',
+        image: { src: imageLeonardKrasner },
+      },
+      {
+        name: 'Blake Reid',
+        role: 'Junior Copywriter',
+        image: { src: imageBlakeReid },
+      },
+      {
+        name: 'Kathryn Murphy',
+        role: 'VP, Human Resources',
+        image: { src: imageKathrynMurphy },
+      },
+      {
+        name: 'Whitney Francis',
+        role: 'Content Specialist',
+        image: { src: imageWhitneyFrancis },
+      },
+      {
+        name: 'Jeffrey Webb',
+        role: 'Account Coordinator',
+        image: { src: imageJeffreyWebb },
+      },
+      {
+        name: 'Benjamin Russel',
+        role: 'Senior Developer',
+        image: { src: imageBenjaminRussel },
+      },
+      {
+        name: 'Angela Fisher',
+        role: 'Front-end Developer',
+        image: { src: imageAngelaFisher },
+      },
+    ],
+  },
+]
+
+function Team() {
+  return (
+    <Container className="mt-24 sm:mt-32 lg:mt-40">
+      <div className="space-y-24">
+        {team.map((group) => (
+          <FadeInStagger key={group.title}>
+            <Border as={FadeIn} />
+            <div className="grid grid-cols-1 gap-6 pt-12 sm:pt-16 lg:grid-cols-4 xl:gap-8">
+              <FadeIn>
+                <h2 className="font-display text-2xl font-semibold text-neutral-950">
+                  {group.title}
+                </h2>
+              </FadeIn>
+              <div className="lg:col-span-3">
+                <ul
+                  role="list"
+                  className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-8"
+                >
+                  {group.people.map((person) => (
+                    <li key={person.name}>
+                      <FadeIn>
+                        <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
+                          <Image
+                            alt=""
+                            {...person.image}
+                            className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
+                          />
+                          <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-40% p-6">
+                            <p className="font-display text-base/6 font-semibold tracking-wide text-white">
+                              {person.name}
+                            </p>
+                            <p className="mt-2 text-sm text-white">
+                              {person.role}
+                            </p>
+                          </div>
+                        </div>
+                      </FadeIn>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </FadeInStagger>
+        ))}
+      </div>
+    </Container>
+  )
+}
+
+export const metadata: Metadata = {
+  title: 'About Us',
+  description:
+    'We believe that our strength lies in our collaborative approach, which puts our clients at the center of everything we do.',
+}
+
+export default async function About() {
+  let blogArticles = (await loadArticles()).slice(0, 2)
+
+  return (
+    <>
+      <PageIntro eyebrow="About us" title="Our strength is collaboration">
+        <p>
+        We believe that our strength lies in our collaborative approach, which
+          puts our clients at the center of everything we do.
+        </p>
+        <div className="mt-10 max-w-2xl space-y-6 text-base">
+          <p>
+          At Motstudio, we don't just build technology; we craft digital masterpieces. Recognized as one of the foremost technology studios globally, our reputation has been sculpted over years of commitment, innovation, and an unwavering pursuit of excellence.
+            </p>
+          <p>Our essence lies in our dedication to premium standards. Each line of code penned, every user interface designed, and every system integrated reflects a meticulous attention to detail, setting us apart in a world where the ordinary often prevails. Our team, composed of luminaries and tech aficionados, approaches every project with a unique blend of cutting-edge technological prowess and creative ingenuity.
+          </p>
+          <p>We're not just about delivering solutions; we aim to create digital experiences that resonate. And it's this drive that has led to collaborations with some of the most visionary brands and forward-thinking enterprises. Our clients choose Motstudio because they seek more than just a service; they seek a partnership that promises reliability, quality, and a touch of the extraordinary.
+          </p>
+          <p>Choosing Motstudio means choosing unparalleled technological craftsmanship, premium quality, and a commitment to pushing the boundaries of what's possible.
+          </p>
+          Join us in our journey of setting the gold standard in technology and design. Because at Motstudio, we believe in crafting tomorrow, today.
+        </div>
+      </PageIntro>
+      <Container className="mt-16">
+        <StatList>
+          <StatListItem value="35" label="Team Members" />
+          <StatListItem value="52" label="Happy Clients" />
+          <StatListItem value="$25M" label="Revenue Generated" />
+        </StatList>
+      </Container>
+
+      <Culture />
+
+      <Team />
+
+      <PageLinks
+        className="mt-24 sm:mt-32 lg:mt-40"
+        title="From the blog"
+        intro="Our team of experienced designers and developers has just one thing on their mind; working on your ideas to draw a smile on the face of your users worldwide. From conducting Brand Sprints to UX Design."
+        pages={blogArticles}
+      />
+
+      <ContactSection />
+    </>
+  )
+}
