4957108e61c6214d62afed3a02cd84c1e7759eb7
Jonathan <jjames422@me.com>
2023-09-06 06:14:04 -0400

    chang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
 import { type Metadata } from 'next'
-
+import AuthProvider from './AuthProvider'
 import { RootLayout } from '@/components/RootLayout'
 
 import '@/styles/tailwind.css'
 
+
 export const metadata: Metadata = {
   title: {
     template: '%s - Studio',
-    default: 'Studio - Award winning developer studio based in Denmark',
+    default: 'Studio - Award winning developer studio based in Charlotte',
   },
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
+    <AuthProvider>
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
-        <RootLayout>{children}</RootLayout>
+          <RootLayout>{children}</RootLayout>
       </body>
     </html>
+    </AuthProvider>
   )
 }
